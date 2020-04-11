879d62579e8e5782531538608bb060b13db61a00
rockerbacon <vitor@librem.one>
2020-04-11 19:21:10 -0300

    Fixed environment setup file copy

--- tests/setup_environment.sh
+++ tests/setup_environment.sh
@@ -13,7 +13,7 @@ fi
 mkdir -p /tmp/foopak_test_environments
 environment_dir=$(mktemp -d /tmp/foopak_test_environments/XXXXXX)
 
-cp -R "$project_root"/* $environment_dir/
+cp -R "$project_root"/* "$project_root"/.[!.]* $environment_dir/
 
 echo "$environment_dir"
 
