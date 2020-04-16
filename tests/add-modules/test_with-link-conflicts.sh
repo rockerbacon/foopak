7b256398b4a4c7f4efeb80a69a4edee55931a856
rockerbacon <vitor@librem.one>
2020-04-16 01:33:07 -0300

    Added link failure check

--- tests/add-modules/test_with-link-conflicts.sh
+++ tests/add-modules/test_with-link-conflicts.sh
@@ -5,8 +5,9 @@ project_root=$(realpath "$(dirname $0)/../..")
 oneTimeSetUp() {
 	environment_dir=$("$project_root/tests/setup_environment.sh")
 	cd $environment_dir
-	mkdir -p foopak_modules/rockerbacon_foopak-mock-module
-	output=$(./foopak add rockerbacon/foopak-mock-module 2>&1); exit_code=$?
+	touch 'script-1'
+	output=$(./foopak add --alias 'mock-alias' rockerbacon/foopak-mock-module 2>&1); \
+		exit_code=$?
 }
 
 oneTimeTearDown() {
@@ -17,12 +18,12 @@ test_should_exit_with_error_code() {
 	assertNotEquals "exited with success code." "0" "$exit_code"
 }
 
-test_should_inform_user_folder_already_exists() {
+test_should_inform_user_link_conflict() {
 	assertNotNull "no output from program" "$output"
 
 	error_message=$( \
 		echo "$output" \
-	|	grep -oiE "could not add module.*'.*/foopak_modules/rockerbacon_foopak-mock-module' already exists" \
+	|	grep -oiE "could not add module.*cannot link '.*/mock-alias/script-1' to '.*/script-1'.*file already exists" \
 	)
 
 	assertNotNull "output message not informative:\n$output" "$error_message"
