e48dfb25e2c034efe6228f1cc090052497bd84af
rockerbacon <vitor@librem.one>
2020-06-14 18:12:21 -0300

    Made adjustments to shellcheck

--- src/constants.sh
+++ src/constants.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-export project_root=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-export escaped_project_root=$(echo "$project_root" | sed 's/\//\\\//g')
+export project_root
+project_root=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+export escaped_project_root
+escaped_project_root=$(echo "$project_root" | sed 's/\//\\\//g')
 
