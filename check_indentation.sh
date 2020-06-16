ea3ebc36643d5c0455aaf356c08c34e83fea1f17
rockerbacon <vitor@librem.one>
2020-06-15 22:11:37 -0300

    Changed indentation check to make local runs easier

--- check_indentation.sh
+++ check_indentation.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
+project_root=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
 check_indentation() {
 	file=$1
+
+	echo "INFO: checking indentation for '$file'" >&2
 
 	space_indentation=$(grep -n -E '^\s* ' "$file")
 
 	if [ "$space_indentation" != "" ]; then
-		echo "ERROR: file '$file' indented with spaces:" >&2
-		echo "$space_indentation" >&2
+		echo "ERROR: file '$file' indented with spaces:"
+		echo "$space_indentation" | tr ' ' '.'
 		return 1
 	fi
 
 	return 0
 }
 
-failure=0
-for file in "$@"; do
-	echo "INFO: checking indentation for '$file'"
-	check_indentation "$file"; exit_status=$?
-	if [ "$exit_status" != "0" ]; then
-		failure=1
-	fi
-done
+failure=$(
+	find "$project_root" \
+		-not -path "$project_root/shunit2/*" \
+		-name '*.sh' |
+	while read -r script; do
+		check_indentation "$script"
+	done
+)
 
-exit $failure
+echo
+if [ -n "$failure" ]; then
+	echo -e "$failure\n"
+	echo "Some files have incorrect indentation"
+	exit 1
+fi
+
+echo "Everything looks fine"
 
