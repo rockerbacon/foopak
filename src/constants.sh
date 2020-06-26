7bd32895ea51524cf239522f7d5dfb72bc73887c
nelson <nlaranjeira01@gmail.com>
2020-06-25 22:00:45 -0300

    Changed project_root constant to point to the first folder containing a .git file on the file hierarchy

--- src/constants.sh
+++ src/constants.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 export project_root
-project_root=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+current_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+finding_project_root=true
+
+while [ "$finding_project_root" == "true" ]; do
+	if [ "$USER" != "$(stat --format=%U "$current_dir")" ]; then
+		echo "Project root directory could not be found."
+		exit 1
+	fi
+	
+	if [ -f "$current_dir/.git" ]; then
+    		finding_project_root=false
+    		project_root="$current_dir"
+    	else
+    		current_dir=$(dirname "$current_dir")
+    	fi
+done
 
 export escaped_project_root
 escaped_project_root=$(echo "$project_root" | sed 's/\//\\\//g')
