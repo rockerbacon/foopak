9ccd56f70acf3b9338f6644aea2f6348396a3c49
rockerbacon <vitor@librem.one>
2020-06-28 17:22:06 -0300

    Added specific locate-cmd tests

---
+++ tests/locate-cmd/executables/test_with-keyword-syntax.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+project_root=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../..")
+
+oneTimeSetUp() {
+	# shellcheck source=./tests/setup_environment.sh
+	source "$project_root/tests/setup_environment.sh"
+
+	mkdir -p foopak_modules/author/mock-module/foopak_meta
+	cat > foopak_modules/author/mock-module/foopak_meta/command_list.conf << EOF
+format-v1
+
+# comment
+script1 path/to/script1
+
+script2 _executable_ nested/path/to/script2
+
+script3 'path/containing spaces/script3'
+
+EOF
+
+	output=$(./foopak locate-cmd script2 2>&1)
+	exit_status=$?
+
+	if [ "$exit_status" == "0" ]; then
+		declare -gA script_location
+		eval script_location="$output"
+	fi
+}
+
+oneTimeTearDown() {
+	teardown_environment
+}
+
+test_should-execute-successfuly() {
+	assertEquals \
+		"exited with status '$exit_status':\n$output\n" \
+		"0" \
+		"$exit_status"
+}
+
+test_should-correctly-determine-module-name() {
+	assertEquals \
+		"author/mock-module" \
+		"${script_location[module_name]}"
+}
+
+test_should-correctly-determine-module-root() {
+	assertEquals \
+		"$test_environment/foopak_modules/author/mock-module" \
+		"${script_location[module_root]}"
+}
+
+test_should-correctly-determine-command() {
+	assertEquals \
+		"('$test_environment/foopak_modules/author/mock-module/nested/path/to/script2')" \
+		"${script_location[cmd]}"
+}
+
+# shellcheck disable=SC1090
+. "$project_root/shunit2/shunit2"
+
---
+++ tests/locate-cmd/executables/test_with-path-containing-spaces.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+project_root=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../..")
+
+oneTimeSetUp() {
+	# shellcheck source=./tests/setup_environment.sh
+	source "$project_root/tests/setup_environment.sh"
+
+	mkdir -p foopak_modules/author/mock-module/foopak_meta
+	cat > foopak_modules/author/mock-module/foopak_meta/command_list.conf << EOF
+format-v1
+
+# comment
+script1 path/to/script1
+
+script2 _executable_ nested/path/to/script2
+
+script3 'path/containing spaces/script3'
+
+EOF
+
+	output=$(./foopak locate-cmd script3 2>&1)
+	exit_status=$?
+
+	if [ "$exit_status" == "0" ]; then
+		declare -gA script_location="$output"
+	fi
+}
+
+oneTimeTearDown() {
+	teardown_environment
+}
+
+test_should-execute-successfuly() {
+	assertEquals \
+		"exited with status '$exit_status':\n$output\n" \
+		"0" \
+		"$exit_status"
+}
+
+test_should-correctly-determine-module-name() {
+	assertEquals \
+		"author/mock-module" \
+		"${script_location[module_name]}"
+}
+
+test_should-correctly-determine-module-root() {
+	assertEquals \
+		"$test_environment/foopak_modules/author/mock-module" \
+		"${script_location[module_root]}"
+}
+
+test_should-correctly-determine-command() {
+	assertEquals \
+		"('$test_environment/foopak_modules/author/mock-module/path/containing spaces/script3')" \
+		"${script_location[cmd]}"
+}
+
+# shellcheck disable=SC1090
+. "$project_root/shunit2/shunit2"
+
